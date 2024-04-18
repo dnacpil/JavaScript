c7379691efdd371b5d68af225bdc5af5f5db8fad
dysiedaisy <dysielnacpil@gmail.com>
2024-04-18 19:31:10 +0100

    DOM & async refresher

---
+++ async_refresher.js
@@ -0,0 +1,3 @@
+/* Write a function called fetchWeatherData that takes a city name as input and 
+makes a GET request to the OpenWeatherMap API (https://openweathermap.org/api) to fetch weather data for that city. 
+Display the temperature and weather description in the console. */
